194f093ce1d9bacb6b9ab24fbf7a796ee50bb016
Megan McLean <425megs@gmail.com>
2022-01-11 20:15:46 -0800

    removed Boostrap stylling, switched to vanilla CSS to simplify, updated README

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 var fetchButton = document.querySelector('.submitBtn')
 var apiKey = "e303b99a14d016cc77ae6aaf2d42686e"
 
-//not working.. I think it needs long/lat data?
 function getApi() {
     var date = moment().format("MMM Do YY")
     var cityName = document.querySelector('#userInput').value
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + cityName + "&appid=" + apiKey;
-
-    //below needs to be hidden until city is searched for    
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + cityName + "&appid=" + apiKey;   
 
     fetch(requestUrl)
         .then(function (response) {
@@ -24,23 +21,6 @@ function getApi() {
             temp.textContent = "Temp: " + data.main.temp
             wind.textContent = "Wind: " + data.wind.speed
             humidity.textContent = "Humidity: " + data.main.humidity
-
-
-        //5 day forecast, design html first
-            var lon = data.coord.lon
-            var lat = data.coord.lat
-            var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon + "&appid=" + apiKey
-                    fetch(oneCallUrl)
-                        .then(function (response) {
-                            return response.json();
-                        })
-                        .then(function (stuff) {
-                            console.log(stuff);
-                        })
         })
     }
 fetchButton.addEventListener('click', getApi);
-
-// think I will need multiple fetch requests, one for the current weather, one for (each?) the 5 day forecast
-
-// localStorage aspect >:( to display previously searched cities
